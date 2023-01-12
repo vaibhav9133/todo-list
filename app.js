4e5be29a64206d81ae2fc5dbcd14c0bdf5c95828
vaibhav9133 <82707642+vaibhav9133@users.noreply.github.com>
2023-01-12 23:40:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,6 +36,6 @@ app.post("/work", function(req, res) {
 app.get("/work", function(req, res) {
     res.render("list", { listTitle: "Work List", newListItems: workItems })
 })
-app.listen(3000, function() {
+app.listen(process.env.PORT||3000, function() {
     console.log("Server started on port 3000 ")
 })
